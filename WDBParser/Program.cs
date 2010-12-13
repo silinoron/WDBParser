f55b8c0a388d83cb5ba68370ac484a36ebb65353
Silinoron <silinoron@gmail.com>
2010-12-12 20:34:35 -0800

    Disable ADB parsing until it is fixed.

--- WDBParser/Program.cs
+++ WDBParser/Program.cs
@@ -14,14 +14,6 @@ namespace WDBParser
                 return;
             }
             var wdbParser = new WDBParser(wdbFolderName);
-
-            string adbFolderName = "adb";
-            if (!Directory.Exists(adbFolderName))
-            {
-                Console.WriteLine("ADB folder does not exist!");
-                return;
-            }
-            var adbParser = new ADBParser(adbFolderName);
         }
     }
 }
